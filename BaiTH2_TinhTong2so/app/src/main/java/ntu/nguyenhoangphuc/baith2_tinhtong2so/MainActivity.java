2ac6851920c4d758ce45502bb44394991dcb5543
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-18 20:24:49 +0700

    BaiTH2_Hoàn Thiện Tính Tổng
    BaiTH2_2: Hoàn thiện Ứng dụng tính tổng 2 số

--- BaiTH2_TinhTong2so/app/src/main/java/ntu/nguyenhoangphuc/baith2_tinhtong2so/MainActivity.java
+++ BaiTH2_TinhTong2so/app/src/main/java/ntu/nguyenhoangphuc/baith2_tinhtong2so/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.nguyenhoangphuc.baith2_tinhtong2so;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    //Xử lý sự kiện click lên nút Tính Tổng
+    public void XuLyCong(View view){
+        //Tìm tham chiếu đến điều khiển trên xử lý
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextSoKQ = findViewById(R.id.edtKQ);
+
+        //Lấy dữ liệu về
+        String dataA = editTextSoA.getText().toString();
+        String dataB = editTextSoB.getText().toString();
+
+        //Chuyển đổi dữ liệu dạng chuỗi sang dạng số nguyên
+        int so_A = Integer.parseInt(dataA);
+        int so_B = Integer.parseInt(dataB);
+
+        //Tính tổng theo yêu cầu
+        int tong = so_A + so_B;
+
+        //Chuyển đổi số nguyên tổng qua dạng chuỗi
+        String dataTong = String.valueOf(tong);
+
+        //In ra màn hình đáp án
+        editTextSoKQ.setText(dataTong);
     }
 }
