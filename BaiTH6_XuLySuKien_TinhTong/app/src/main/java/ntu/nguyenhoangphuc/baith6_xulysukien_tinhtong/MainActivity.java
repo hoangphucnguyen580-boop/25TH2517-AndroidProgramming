b2a28ffcd50b1a398fdc901ce0036e97d45ee20e
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-20 21:48:39 +0700

    BaiTH6_XuLySuKien_TinhTong
    Hoàn Thiện

--- BaiTH6_XuLySuKien_TinhTong/app/src/main/java/ntu/nguyenhoangphuc/baith6_xulysukien_tinhtong/MainActivity.java
+++ BaiTH6_XuLySuKien_TinhTong/app/src/main/java/ntu/nguyenhoangphuc/baith6_xulysukien_tinhtong/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.nguyenhoangphuc.baith6_xulysukien_tinhtong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void XuLyCong (View view){
+        EditText editTextSoA = findViewById(R.id.etSoA);
+        EditText editTextSoB = findViewById(R.id.etSoB);
+        EditText editTextSoKQ = findViewById(R.id.etkq);
+
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        int Tong = so_A + so_B;
+        String strTong = String.valueOf(Tong);
+
+        editTextSoKQ.setText(strTong);
     }
 }
